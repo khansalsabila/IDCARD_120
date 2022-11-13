b87dad0a6141c9e8c19092e2997f50bcc191f11e
khansalsabila <lathifahks1702@gmail.com>
2022-11-13 21:09:21 +0700

    menambahkan index

---
+++ src/main/java/pwstugas/IdCard/IdCardApplication.java
@@ -0,0 +1,13 @@
+package pwstugas.IdCard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdCardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pwstugas/IdCard/IdCardApplicationTests.java
@@ -0,0 +1,13 @@
+package pwstugas.IdCard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
